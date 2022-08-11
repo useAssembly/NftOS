4361672a7df36edecb901eb2171524ade4c73b75
jeraldgan <jeraldgantc@gmail.com>
2022-08-11 15:50:29 +0800

    feat: add footer

---
+++ src/common/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const Footer = () => (
+  <Box bottom="0px" pb="16px" pl="148px" width="100%">
+    <Text color="gray.400" fontSize="1rem" lineHeight="1.5rem">
+      © Airfoil nftOS
+    </Text>
+  </Box>
+);
