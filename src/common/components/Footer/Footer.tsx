9a03f4d63b2ddbcaeda78016da78eaca9473d385
jeraldgan <jeraldgantc@gmail.com>
2022-08-11 16:40:40 +0800

    chore: should be size instead of variant

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const Footer = () => (
   <Box bottom="0px" pb="16px" pl="148px" width="100%">
-    <Text color="gray.400" variant="md">
+    <Text color="gray.400" size="md">
       © Airfoil nftOS
     </Text>
   </Box>
