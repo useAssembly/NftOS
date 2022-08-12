e1703e8acc80cd448280cac11add89948c7166aa
Khang Nguyen <nguyenak95@gmail.com>
2022-08-12 15:12:58 +0700

    add more color extracted from figma

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,11 +2,29 @@ import { Colors } from "@chakra-ui/react";
 
 export const colors: Colors = {
   gray: {
+    200: "#E2E8F0",
     300: "#CBD5E0",
     400: "#A0AEC0",
+    500: "#718096",
+    600: "#4A5568",
+    700: "#2D3748",
+    800: "#1A202C",
     900: "#171923",
   },
   teal: {
     500: "#319795",
+  },
+  green: {
+    500: "#38A169",
+  },
+  blue: {
+    100: "#BEE3F8",
+    500: "#3182CE",
+  },
+  purple: {
+    400: "#9F7AEA",
+  },
+  yellow: {
+    500: "#D69E2E",
   },
 };
