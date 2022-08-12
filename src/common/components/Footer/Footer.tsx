eba08a827ec0da5c8763444dc0c4ac26f1fae205
jeraldgan <jeraldgantc@gmail.com>
2022-08-12 11:48:32 +0800

    feat: add fixed to footer

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { Box, Text } from "@chakra-ui/react";
 
 export const Footer = () => (
-  <Box bottom="0px" pb="16px" pl="148px" width="100%">
+  <Box bottom="0px" pb="16px" pl="148px" position="fixed" width="100%">
     <Text color="gray.400" size="md">
       © Airfoil nftOS
     </Text>
