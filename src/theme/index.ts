e2671df29bc0419deeadbddd4c1c4058a811b7b5
Andre H <andrehadiantolesmana@gmail.com>
2022-08-15 08:53:55 +0800

    chore: update eslint

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import { colors } from "./colors";
-import { fonts } from "./fonts";
+import { Button } from "./components/button";
 import { Heading } from "./components/heading";
 import { Text } from "./components/text";
-import { Button } from "./components/button";
+import { fonts } from "./fonts";
 
 const theme = extendTheme({
   components: {
