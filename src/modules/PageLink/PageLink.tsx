221b6edcc236eb1da64c9fd9d4575d7da1cd981c
Andre H <andrehadiantolesmana@gmail.com>
2022-08-15 09:23:25 +0800

    feat: add link Href

--- src/modules/PageLink/PageLink.tsx
+++ src/modules/PageLink/PageLink.tsx
@@ -1,9 +1,10 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Link, Text } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 
 import { Card, Content, Header } from "@/common/components/Card";
 
 interface Props {
+  linkHref: string;
   icon: JSX.Element;
   label: string;
   description: string;
@@ -11,38 +12,41 @@ interface Props {
 }
 
 export const PageLink: FunctionComponent<Props> = ({
+  linkHref,
   icon,
   label,
   description,
   size = "md",
 }) => {
   return (
-    <Card
-      borderRadius={size === "md" ? 16 : 12}
-      paddingX={3}
-      paddingY={size === "md" ? 3 : 2}
-      width={size === "md" ? "200px" : "fit-content"}
-    >
-      {size === "md" ? (
-        <>
-          <Header marginBottom={"36px"}>{icon}</Header>
+    <Link href={linkHref}>
+      <Card
+        borderRadius={size === "md" ? 16 : 12}
+        paddingX={3}
+        paddingY={size === "md" ? 3 : 2}
+        width={size === "md" ? "200px" : "fit-content"}
+      >
+        {size === "md" ? (
+          <>
+            <Header marginBottom={"36px"}>{icon}</Header>
+            <Content>
+              <Text size="xxxl">{label}</Text>
+              <Text color="gray.500" size="sm">
+                {description}
+              </Text>
+            </Content>
+          </>
+        ) : (
           <Content>
-            <Text size="xxxl">{label}</Text>
-            <Text color="gray.500" size="sm">
-              {description}
-            </Text>
+            <HStack spacing={2}>
+              <Box>{icon}</Box>
+              <Box>
+                <Text size="xl">{label}</Text>
+              </Box>
+            </HStack>
           </Content>
-        </>
-      ) : (
-        <Content>
-          <HStack spacing={2}>
-            <Box>{icon}</Box>
-            <Box>
-              <Text size="xl">{label}</Text>
-            </Box>
-          </HStack>
-        </Content>
-      )}
-    </Card>
+        )}
+      </Card>
+    </Link>
   );
 };
