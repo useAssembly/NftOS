36f09965d00fbcbd7df6c23524bf9eff0443e407
Andre H <andrehadiantolesmana@gmail.com>
2022-08-18 07:39:02 +0800

    feat: add mint pagelink

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
+import { Flex, Heading, HStack } from "@chakra-ui/react";
+
+import { LightningIcon } from "@/common/components/CustomIcon";
 import { PageHead } from "@/common/components/PageHead";
+
+import { PageLink } from "@/modules/PageLink";
 
 export default function Home() {
   return (
@@ -8,9 +13,19 @@ export default function Home() {
         description="Home page description"
         name="Home"
       />
-      <section className="grid place-content-center h-full">
-        <span>Hello world</span>
-      </section>
+      <Flex flexDirection={"column"} pt="52px" px="148px">
+        <Heading fontWeight={600} mb={4} size={"2xl"}>
+          Where do you want to go?
+        </Heading>
+        <HStack>
+          <PageLink
+            description="Mint new NFTs"
+            icon={<LightningIcon />}
+            label="Mint"
+            linkHref="/mint"
+          />
+        </HStack>
+      </Flex>
     </div>
   );
 }
