eac08177b64f0d940d6134168744b7192a610180
jeraldgan <jeraldgantc@gmail.com>
2022-08-18 12:40:37 +0800

    chore: use specific naming to Statistic

--- src/modules/StatisticCard/StatisticCard.tsx
+++ src/modules/StatisticCard/StatisticCard.tsx
@@ -4,31 +4,31 @@ import { FunctionComponent } from "react";
 import { Card, Content, Footer, Header } from "@/common/components/Card";
 
 interface Props {
-  header: string | React.ReactNode;
-  content: string | React.ReactNode;
-  footer: string | React.ReactNode;
+  title: string | React.ReactNode;
+  mainStats: string | React.ReactNode;
+  footerStats: string | React.ReactNode;
 }
 
 export const StatisticCard: FunctionComponent<Props> = ({
-  header,
-  content,
-  footer,
+  title,
+  mainStats,
+  footerStats,
 }) => {
   return (
     <Card px={6} py={5}>
       <Header minWidth={177} textAlign="left">
         <Text color="gray.700" fontWeight={500} size="sm">
-          {header}
+          {title}
         </Text>
       </Header>
       <Content minWidth={177} mt={6} textAlign="left">
         <Text color="gray.700" fontWeight={500} size="4xl">
-          {content}
+          {mainStats}
         </Text>
       </Content>
       <Footer minWidth={177} mt={1} textAlign="left">
         <Text color="gray.700" fontWeight={500} opacity={0.8} size="sm">
-          {footer}
+          {footerStats}
         </Text>
       </Footer>
     </Card>
