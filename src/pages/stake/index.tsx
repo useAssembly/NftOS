b09d903bcc116f18875ba41311f38f03330871ee
Andre H <andrehadiantolesmana@gmail.com>
2022-08-19 14:28:41 +0800

    chore: use statistic card

--- src/pages/stake/index.tsx
+++ src/pages/stake/index.tsx
@@ -3,6 +3,7 @@ import { Box, Flex, Heading, HStack } from "@chakra-ui/react";
 import { PageHead } from "@/common/components/PageHead";
 
 import { StakedNFT } from "@/modules/\bStakedNFT";
+import { StatisticCard } from "@/modules/StatisticCard";
 import { UnstakedNFT } from "@/modules/UnstakedNFT";
 
 const StakePage = () => {
@@ -19,17 +20,21 @@ const StakePage = () => {
             Your $AFP stats
           </Heading>
           <HStack spacing={3}>
-            {Array()
-              .fill("_")
-              .map((_, i) => (
-                <Box
-                  key={i}
-                  bg={"coral"}
-                  borderRadius={12}
-                  height={"148px"}
-                  width={"225px"}
-                />
-              ))}
+            <StatisticCard
+              footerStats="1052 staked"
+              label="Total Staked"
+              mainStats="60.6%"
+            />
+            <StatisticCard
+              footerStats="01h 22m to next payout"
+              label="Daily returns"
+              mainStats="1 $AFP"
+            />
+            <StatisticCard
+              footerStats="Estimated: 3 $AIR"
+              label="Your staked"
+              mainStats="3 $AFP"
+            />
           </HStack>
         </Box>
         <Box mb={12}>
