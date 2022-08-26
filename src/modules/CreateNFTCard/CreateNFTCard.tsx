640567bc173ecfc2482a649b3f7bc70328fcfd1b
Khang Nguyen <nguyenak95@gmail.com>
2022-08-26 20:28:38 +0700

    add CreateNFT component

---
+++ src/modules/CreateNFTCard/CreateNFTCard.tsx
@@ -0,0 +1,36 @@
+import { Center, Heading, Button, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+import { Card, Content, Footer } from "@/common/components/Card";
+
+export const CreateNFTCard = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    window.open(
+      "https://thirdweb.com/dashboard",
+      "_blank",
+      "noopener,noreferrer"
+    );
+    router.push("/deploy/info");
+  };
+  return (
+    <Card maxWidth={476} minWidth={{ base: "full", md: 476 }}>
+      <Content>
+        <Center>
+          <Heading pb={1}>Create NFT Contract</Heading>
+        </Center>
+        <Text color="gray.700" size="sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </Text>
+      </Content>
+      <Footer>
+        <Button width="full" onClick={handleClick}>
+          <Link href="/deploy/info">Create with Thirdweb</Link>
+        </Button>
+      </Footer>
+    </Card>
+  );
+};
