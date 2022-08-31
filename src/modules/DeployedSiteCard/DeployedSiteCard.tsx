535562cf07b80a307f61342dbc167e9ef114e2fc
Khang Nguyen <nguyenak95@gmail.com>
2022-08-31 11:13:55 +0700

    update deploy site detail page

--- src/modules/DeployedSiteCard/DeployedSiteCard.tsx
+++ src/modules/DeployedSiteCard/DeployedSiteCard.tsx
@@ -1,27 +1,46 @@
-import { Button, Center, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Link from "next/link";
 
 import { Card, Content, Footer, Header } from "@/common/components/Card";
 
-const sites = ["https://nftmint.xyz"];
+const sites = [
+  { name: "nftmint.xyz", url: "https://nftmint.xyz" },
+  { name: "opensea.io", url: "https://opensea.io/" },
+];
 
 export const DeployedSiteCard = () => {
   return (
     <Card maxWidth={476} minWidth={{ base: "full", md: 476 }}>
       <Content>
         <Header>
-          <Center>
-            <Heading pb={4}>Deployed Site</Heading>
-          </Center>
+          <VStack textAlign="center">
+            <Heading pb={4}>Deployed Site Details</Heading>
+            <Text>Deployment Status: Pending</Text>
+          </VStack>
         </Header>
         <Center>
           <VStack>
             {sites.map((item, i) => (
-              <Link key={i} passHref href={item}>
-                <Text color="blue.500" cursor="pointer" decoration="underline">
-                  {item}
-                </Text>
-              </Link>
+              <HStack key={i}>
+                <Text>Site: </Text>
+                <Link passHref href={item.url}>
+                  <Text
+                    color="blue.500"
+                    cursor="pointer"
+                    decoration="underline"
+                  >
+                    {item.name}
+                  </Text>
+                </Link>
+              </HStack>
             ))}
           </VStack>
         </Center>
